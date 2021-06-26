8770ac13584e2a70267282b7a7c851047f04da86
Prem Kantikar <48849830+alcatraz69@users.noreply.github.com>
2021-06-26 11:08:39 +0530

    chore - updated footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,22 +1,43 @@
-import './Footer.css'
+import "./Footer.css";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-export default function Footer(){
-    return(
-        <div className="footer">
-            <div className="footer-header">
-              Made with <span style={{color:"red"}}> ❤ </span> by
-              Prem Kantikar
-            </div>
-            <div className="social-links">
-                <a className="icon" href="https://github.com/alcatraz69" target="_blank" rel="noreferrer"> <FaGithub/></a>
-                <a className="icon" href="https://twitter.com/prem_kantikar_" target="_blank" rel="noreferrer"> <FaTwitter/> </a>
-                <a className="icon" href="https://twitter.com/prem_kantikar_" target="_blank" rel="noreferrer"> <FaLinkedinIn/></a>
-            </div>
-            <div className="footer-end">
-                © 2021 Alcatraz Designs
-           </div>
-        </div>
-    )
+import { GrTwitter } from "react-icons/gr";
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-header">
+        Made with <span style={{ color: "red" }}> ❤ </span> by Prem Kantikar
+      </div>
+      <div className="social-links">
+        <a
+          className="icon"
+          href="https://github.com/alcatraz69"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          <FaGithub />
+        </a>
+        <a
+          className="icon"
+          href="https://twitter.com/prem_kantikar_"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          <GrTwitter />{" "}
+        </a>
+        <a
+          className="icon"
+          href="https://twitter.com/prem_kantikar_"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          <FaLinkedinIn />
+        </a>
+      </div>
+      <div className="footer-end">© 2021 Alcatraz Designs</div>
+    </div>
+  );
 }
